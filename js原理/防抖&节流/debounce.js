206fd92d8c82ffb38fb5800ed7765505d2fd6450
JYL827 <1345044954@qq.com>
2020-11-16 21:18:33 +0800

    js原理

--- js原理/防抖&节流/debounce.js
+++ js原理/防抖&节流/debounce.js
@@ -1,1 +1,10 @@
-// 在规定的时间内
+// 在规定的时间内如果重复触发事件，则清除定时器，等待不触发时再发起请求
+function debounce(fn) {
+  let timeout
+  return function() {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      fn()
+    }, 2000)
+  }
+}
